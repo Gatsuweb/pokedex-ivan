281fa2a6c971fdf1a21454d517d1f64ced1a7f35
gatsuweb <ivan.duran@outlook.fr>
2023-12-13 17:05:13 +0100

    React basics 06 fini

--- pokedex-ivan/src/App.jsx
+++ pokedex-ivan/src/App.jsx
@@ -3,7 +3,7 @@ import PokemonCard from './components/PokemonCard'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  
   const pokemonList = [
     {
       name: "Salamèche",
@@ -11,24 +11,54 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
     },
     {
-      name: "Mew",
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-      name: "Carapuce",
-          imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-  
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
   ];
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const btnPrecedent = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+
+  const btnSuivant = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
 
   return (
     <>
       <div>
-        <PokemonCard pokemon = {pokemonList[0]}/>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+
+        {pokemonIndex > 0 && (
+          <button onClick={btnPrecedent}>Précédent</button>
+        )}
+
+        {pokemonIndex < pokemonList.length - 1 && (
+          <button onClick={btnSuivant}>Suivant</button>
+        )}
       </div>
-      
     </>
-  )
+  );
 }
 
 export default App
