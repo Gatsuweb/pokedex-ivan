36dc649ebd17b7ef6a00d83587f9ff3f77d3954f
gatsuweb <ivan.duran@outlook.fr>
2023-12-15 21:24:21 +0100

    Choisi ton starter

--- pokedex-ivan/src/App.jsx
+++ pokedex-ivan/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PokemonCard from './components/PokemonCard';
 import NavBar from './components/NavBar';
 import './App.css'
 
 function App() {
-  
+
   const pokemonList = [
     {
       name: "Salamèche",
@@ -30,13 +30,37 @@ function App() {
       name: "mew",
     },
   ];
-  const [pokemonIndex, setPokemonIndex] = useState(0);
 
+  useEffect(() => {
+    alert("Choose your starter !")
+}, []);
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
   const handlePokemonCLick = (pokemonIndex) => {
     
     setPokemonIndex(pokemonIndex);
-  
+
+    if (pokemonIndex === 0) {
+      alert("J'suis le starter préféré des gens choisi moi !!!");
+    }
+
+    if (pokemonIndex === 1) {
+      alert("J'ai un bourgeon sur le dos mais je suis mim's, regarde ce sourire !");
+    }
+
+    if (pokemonIndex === 2) {
+      alert("Je suis la version évoluée d'une tortue !");
+    }
+
+    if (pokemonIndex === 3) {
+      alert("pika pikachu !!!");
+    }
+
+    if (pokemonIndex === 4) {
+      alert("J'suis pas un starter bouge !!!");
+    }
+   
 };
 
   return (
--- pokedex-ivan/src/components/NavBar.jsx
+++ pokedex-ivan/src/components/NavBar.jsx
@@ -4,14 +4,16 @@ function NavBar ({handlePokemonCLick, pokemonList }) {
 return (
     <>
     {pokemonList.map ((pokemonList, index) => (
-
+     
       <button key={index} 
       onClick={ () => handlePokemonCLick(index)}>
       {pokemonList.name}
       </button>
-    )
+      
+      )
 
     )}
+    
  </>
 )}
 
